32af0b4a2118ea622b284d5739950138c793e9d2
coltw27 <cwilmouth27@yahoo.com>
2024-10-17 15:55:49 -0400

    add showmodal method

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -3,20 +3,19 @@ import { enter, leave } from 'el-transition'
 
 export default class extends Controller {
   connect() {
-    enter(document.getElementById('authentication-modal'))
-    enter(document.getElementById('auth-modal-wrapper'))
-
-    // document.getElementById('authentication-modal').addEventListener('click', this.closeModal)
     document.getElementById('auth-modal-wrapper').addEventListener('click', this.closeModal)
+    document.getElementById('authentication-modal').addEventListener('click', this.showModal)
   }
 
   closeModal(event) {
     const modalPanelClicked = document.getElementById('authentication-modal').contains(event.target);
-    console.log(modalPanelClicked)
-  
+
     if (!modalPanelClicked) {
-      console.log("leaving")
       leave(document.getElementById('authentication-modal'))
     }
+  }
+
+  showModal(event) {
+    enter(document.getElementById('authentication-modal'));
   }
 }
