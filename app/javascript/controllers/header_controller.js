c1d7da948efb38041e7f602faf4498427b08c020
coltw27 <cwilmouth27@yahoo.com>
2024-10-17 17:06:14 -0400

    Add close functionality with x to modal

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -2,13 +2,27 @@ import { Controller } from '@hotwired/stimulus'
 import { toggle } from 'el-transition'
 
 export default class extends Controller {
-  static targets = ['openUserMenu', 'userAuthLink']
+  static targets = [
+    'openUserMenu',
+    'userAuthLink',
+    'profileDropdownLinks'
+  ]
+
   connect() {
-    this.openUserMenuTarget.addEventListener('click', this.toggleDropdownMenu)
+    this.openUserMenuTarget.addEventListener('click', this.toggleDropdownMenu.bind(this))
 
     this.userAuthLinkTargets.forEach((link) => {
       link.addEventListener('click', (e) => {
         document.getElementById('modal-trigger').click();
+        this.toggleDropdownMenu()
+       
+      });
+    });
+    
+    this.profileDropdownLinksTargets.forEach((link) => {
+      link.addEventListener('click', (e) => {
+        this.toggleDropdownMenu()
+       
       });
     });
   }; 
--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -3,14 +3,17 @@ import { enter, leave } from 'el-transition'
 
 export default class extends Controller {
   connect() {
+    document.getElementById('auth-modal-close-button').addEventListener('click', this.closeModal)
     document.getElementById('auth-modal-wrapper').addEventListener('click', this.closeModal)
     document.getElementById('authentication-modal').addEventListener('click', this.showModal)
   }
 
   closeModal(event) {
     const modalPanelClicked = document.getElementById('authentication-modal').contains(event.target);
+    console.log(modalPanelClicked)
 
-    if (!modalPanelClicked) {
+    if (!modalPanelClicked || event.target.id === 'auth-modal-close-button') {
+      console.log('hit the x')
       leave(document.getElementById('authentication-modal'))
     }
   }
@@ -18,4 +21,5 @@ export default class extends Controller {
   showModal(event) {
     enter(document.getElementById('authentication-modal'));
   }
+
 }
